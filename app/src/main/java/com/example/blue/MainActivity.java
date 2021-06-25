712794489726a992de87f9ca1b2a9cdde320d24c
Ahra Cho <choar816@postech.ac.kr>
2021-06-26 00:29:02 +0900

    main & some part of menu1

--- app/src/main/java/com/example/blue/MainActivity.java
+++ app/src/main/java/com/example/blue/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.blue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1 = (Button) findViewById(R.id.button1);
+    Button button2 = (Button) findViewById(R.id.button2);
+    Button button3 = (Button) findViewById(R.id.button3);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Menu1Activity.class);
+                startActivity(intent);
+            }
+        });
+/*
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });*/
     }
 }
---
+++ app/src/main/java/com/example/blue/Menu1Activity.java
@@ -0,0 +1,55 @@
+package com.example.blue;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class Menu1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.menu1_viewpager);
+        pager.setOffscreenPageLimit(3);
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+
+        Menu1Fragment1 fragment1 = new Menu1Fragment1();
+        adapter.addItem(fragment1);
+        Menu1Fragment2 fragment2 = new Menu1Fragment2();
+        adapter.addItem(fragment2);
+        Menu1Fragment3 fragment3 = new Menu1Fragment3();
+        adapter.addItem(fragment3);
+
+    }
+
+    class PagerAdapter extends FragmentStatePagerAdapter {
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item) {
+            items.add(item);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+    }
+}
